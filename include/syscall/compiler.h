6147d3889ddbd18f6bd2730c41cd9b9590587af1
Minimob <68860403+minimob1337@users.noreply.github.com>
2026-06-09 21:31:13 +0200

    fix: clang compiler check

--- include/syscall/compiler.h
+++ include/syscall/compiler.h
@@ -1,12 +1,12 @@
 #pragma once
 
 // portable compiler macros
-#if defined(_MSC_VER)
-    #define SYSCALL_FORCEINLINE __forceinline
-    #define SYSCALL_CALLCONV __stdcall
-#elif defined(__GNUC__) || defined(__clang__)
+#if defined(__GNUC__) || defined(__clang__)
     #define SYSCALL_FORCEINLINE __attribute__((always_inline)) inline
     #define SYSCALL_CALLCONV
+#elif defined(_MSC_VER)
+    #define SYSCALL_FORCEINLINE __forceinline
+    #define SYSCALL_CALLCONV __stdcall
 #else
     #define SYSCALL_FORCEINLINE inline
     #define SYSCALL_CALLCONV
